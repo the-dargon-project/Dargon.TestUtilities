a4967d971e02ae72f4ead2dac470eaa7127055ea
Michael Yu <ItzWarty@gmail.com>
2014-11-20 13:27:51 -0800

    Update AssemblyInfo in preparation for NuGet Release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dargon.TestUtilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dargon Test Utilities!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("Dargon.TestUtilities")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ItzWarty 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
