0497e51dac49f0ebd920dce9a9e925e91be678cb
Michael Yu <ItzWarty@gmail.com>
2014-11-20 13:24:56 -0800

    Initial Commit.

---
+++ PofTestUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Dargon.PortableObjects;
+using NMockito;
+
+namespace Dargon {
+   public static class PofTestUtilities {
+      public static void CheckConfiguration<TPortableObject>(IPofContext context, TPortableObject testObj) 
+         where TPortableObject : IPortableObject {
+         var serializer = new PofSerializer(context);
+         using (var ms = new MemoryStream()) {
+            serializer.Serialize(ms, testObj);
+            ms.Position = 0;
+            var deserialized = serializer.Deserialize<TPortableObject>(ms);
+            NMockitoStatic.AssertEquals(testObj, deserialized);
+         }
+      }
+   }
+}
